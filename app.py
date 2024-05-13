e5387e9a7c14e7a5ed8c5e9a98562a5624b1be9e
mrcloudchase <mrcloudchase@gmail.com>
2024-05-13 17:37:42 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+import socket
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_hostname():
+    hostname = socket.gethostname()
+    endpoint = request.endpoint
+    return jsonify({"hostname": hostname, "endpoint": endpoint})
+
+if __name__ == '__main__':
+    app.run(debug=True)
